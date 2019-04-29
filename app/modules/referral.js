cf36d9c4da6b2a4733c4133c0b9e7720a42ba919
sauasmast <sdahal@caldwell.edu>
2019-04-28 23:40:38 -0400

    Create data function added

--- app/modules/referral.js
+++ app/modules/referral.js
@@ -59,25 +59,26 @@ module.exports.userreferreddata = (request_id, user_id) => {
     }
   });
 };
+
 // This one was for creating the data if the user exist but the user does not exist in referral table
-// module.exports.createdata = (request_id, user_data) => {
-//   return new Promise(async (resolve, reject) => {
-//     const data = {
-//       user_id: user_data.user_id,
-//       email: user_data.email,
-//       status: 'active',
-//       source: user_data.referred_by_user_id ? 'referral' : 'web',
-//       reference_id: user_data.reference_id,
-//       referred_by_user_id: user_data.referred_by_user_id
-//     };
-//     let queryString = `INSERT INTO users SET ?`;
-//     try {
-//       let result = await mysql.query(request_id, db, queryString, data);
-//       if (result.affectedRows == 1) {
-//         resolve();
-//       }
-//     } catch (e) {
-//       reject({ code: 102, message: { message: e.message, stack: e.stack } });
-//     }
-//   });
-// };
+module.exports.createdata = (request_id, user_data) => {
+  return new Promise(async (resolve, reject) => {
+    const data = {
+      user_id: user_data.user_id,
+      email: user_data.email,
+      status: 'active',
+      source: user_data.referred_by_user_id ? 'referral' : 'web',
+      reference_id: user_data.reference_id,
+      referred_by_user_id: user_data.referred_by_user_id
+    };
+    let queryString = `INSERT INTO users SET ?`;
+    try {
+      let result = await mysql.query(request_id, db, queryString, data);
+      if (result.affectedRows == 1) {
+        resolve();
+      }
+    } catch (e) {
+      reject({ code: 102, message: { message: e.message, stack: e.stack } });
+    }
+  });
+};
